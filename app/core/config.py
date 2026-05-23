23e60995941ef51d231542a62eecffa977c3cdbe
Madhav Arora <eragon.cube@outlook.com>
2026-05-23 18:45:26 +0530

    add ephemeris path in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,10 @@
-"""Placeholder File"""
+import os
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Config:
+    ephe_path: str = os.getenv("EPHE_PATH", "./ephe")
+
+
+config = Config()
--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ from datetime import datetime, timezone
 
 import swisseph as swe
 
+from app.core.config import config
+
 if __name__ == "__main__":
-    swe.set_ephe_path("./ephe")
+    swe.set_ephe_path(config.ephe_path)
     now = datetime.now(timezone.utc)
 
     jd_ut, jd_tt = swe.utc_to_jd(
